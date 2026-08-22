7bca7324b7b1e127070f3bdcab71ac6fd6762295
Tyler Worth <tylerworth.media@gmail.com>
2026-08-22 12:12:24 +0100

    Fix robots.txt route

--- anarchy_and_lace/urls.py
+++ anarchy_and_lace/urls.py
@@ -1,8 +1,9 @@
 from django.conf import settings
 from django.conf.urls.static import static
+
 from django.contrib import admin
-from django.urls import include, path
 from django.contrib.sitemaps.views import sitemap
+from django.urls import include, path
 from django.views.generic import TemplateView
 
 from .sitemaps import (
@@ -11,28 +12,48 @@ from .sitemaps import (
     StaticSitemap,
 )
 
+
 sitemaps = {
     "static": StaticSitemap,
     "collections": CollectionSitemap,
     "products": ProductSitemap,
 }
 
+
 urlpatterns = [
     path("admin/", admin.site.urls),
+
     path("shop/", include("catalog.urls")),
-    path("sitemap.xml", sitemap, {"sitemaps": sitemaps}, name="django.contrib.sitemaps.views.sitemap"),
-    path("", include(("home.urls", "home"), namespace="home")),
-    path("analytics/", include("analytics.urls"),
-),
+
+    path(
+        "sitemap.xml",
+        sitemap,
+        {"sitemaps": sitemaps},
+        name="django.contrib.sitemaps.views.sitemap",
+    ),
+
+    path(
+        "robots.txt",
+        TemplateView.as_view(
+            template_name="robots.txt",
+            content_type="text/plain",
+        ),
+    ),
+
+    path(
+        "analytics/",
+        include("analytics.urls"),
+    ),
+
+    path(
+        "",
+        include(("home.urls", "home"), namespace="home"),
+    ),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-path(
-    "robots.txt",
-    TemplateView.as_view(
-        template_name="robots.txt",
-        content_type="text/plain",
-    ),
-),
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT,
+    )
